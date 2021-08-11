321f1fd2c880b22b52964d62d4bc073b16e1d6f8
krazijames <krazijames@gmail.com>
2021-08-11 23:01:40 +0900

    Clone `onnxruntime` on build

--- build.sh
+++ build.sh
@@ -2,12 +2,13 @@
 
 # Reference: https://github.com/microsoft/onnxruntime/blob/v1.6.0/BUILD.md#cross-compiling-on-linux
 
-BUILD_DIR=./build
-INSTALL_DIR=./dist
-
-git submodule update --init --recursive
+ONNXRUNTIME_TAG=v1.6.0
+BUILD_DIR=./build/$ONNXRUNTIME_TAG
+INSTALL_DIR=./dist/$ONNXRUNTIME_TAG
 
 export PATH="$(pwd)/gcc-arm-10.3-2021.07-x86_64-aarch64-none-linux-gnu/bin:$PATH"
+
+git clone --branch $ONNXRUNTIME_TAG --single-branch --recursive --shallow-submodules https://github.com/Microsoft/onnxruntime
 
 cmake \
     -S ./onnxruntime/cmake \
