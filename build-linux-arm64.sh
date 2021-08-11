e12e417129927153ed07799816f29b010ca9e9e9
krazijames <krazijames@gmail.com>
2021-08-11 23:31:35 +0900

    Rename files & directories

--- build-linux-arm64.sh
+++ build-linux-arm64.sh
@@ -3,15 +3,19 @@
 # Reference: https://github.com/microsoft/onnxruntime/blob/v1.6.0/BUILD.md#cross-compiling-on-linux
 
 ONNXRUNTIME_TAG=v1.6.0
-BUILD_DIR=./build/$ONNXRUNTIME_TAG
-INSTALL_DIR=./dist/$ONNXRUNTIME_TAG
+OS=linux
+ARCH=arm64
+TARGET=$OS-$ARCH
+CLONE_DIR=./onnxruntime-$ONNXRUNTIME_TAG
+BUILD_DIR=./build/onnxruntime-$TARGET-$ONNXRUNTIME_TAG
+INSTALL_DIR=./dist/onnxruntime-$TARGET-$ONNXRUNTIME_TAG
 
 export PATH="$(pwd)/gcc-arm-10.3-2021.07-x86_64-aarch64-none-linux-gnu/bin:$PATH"
 
-git clone --branch $ONNXRUNTIME_TAG --single-branch --recursive --shallow-submodules https://github.com/Microsoft/onnxruntime
+git clone --branch $ONNXRUNTIME_TAG --single-branch --recursive --shallow-submodules https://github.com/Microsoft/onnxruntime $CLONE_DIR
 
 cmake \
-    -S ./onnxruntime/cmake \
+    -S $CLONE_DIR/cmake \
     -B $BUILD_DIR \
     -DONNX_CUSTOM_PROTOC_EXECUTABLE="$(pwd)/protoc-3.11.2-linux-x86_64/bin/protoc" \
     -DCMAKE_TOOLCHAIN_FILE="$(pwd)/tool.cmake" \
