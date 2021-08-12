fa5b55f965e0ec9d283f5aa7c622ab0a0735da15
krazijames <krazijames@gmail.com>
2021-08-12 18:08:04 +0900

    Build using CMake

--- build-linux-arm64.sh
+++ build-linux-arm64.sh
@@ -2,20 +2,19 @@
 
 # Reference: https://github.com/microsoft/onnxruntime/blob/v1.6.0/BUILD.md#cross-compiling-on-linux
 
+set -e
+
 VERSION=1.6.0
 OS=linux
 ARCH=arm64
-CLONE_DIR=./onnxruntime-$VERSION
-BUILD_DIR=./build/onnxruntime-$OS-$ARCH-$VERSION
+BUILD_DIR=./build/onnxruntime-$OS-$ARCH
 INSTALL_DIR=./dist/onnxruntime-$OS-$ARCH-$VERSION
-
-export PATH="$(pwd)/gcc-arm-10.3-2021.07-x86_64-aarch64-none-linux-gnu/bin:$PATH"
-
-git clone --branch v$VERSION --single-branch --recursive --shallow-submodules https://github.com/Microsoft/onnxruntime $CLONE_DIR
+PATH="$(pwd)/gcc-arm-10.3-2021.07-x86_64-aarch64-none-linux-gnu/bin:$PATH"
 
 cmake \
-    -S $CLONE_DIR/cmake \
+    -S . \
     -B $BUILD_DIR \
+    -DVERSION=$VERSION \
     -DONNX_CUSTOM_PROTOC_EXECUTABLE="$(pwd)/protoc-3.11.2-linux-x86_64/bin/protoc" \
     -DCMAKE_TOOLCHAIN_FILE="$(pwd)/tool.cmake" \
     -Donnxruntime_BUILD_SHARED_LIB=ON \
