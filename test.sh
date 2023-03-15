de79dbbfbe2b93cadf1090c894a340a1052b0e2e
krazijames <krazijames@gmail.com>
2023-03-16 00:10:37 +0900

    Remove unnecessary CMake configs

--- test.sh
+++ test.sh
@@ -8,10 +8,9 @@ BUILD_DIR=$SOURCE_DIR/build
 CMAKE_OPTIONS="\
     -S $SOURCE_DIR \
     -B $BUILD_DIR \
-    -D CMAKE_BUILD_TYPE=Release \
     $TEST_CMAKE_OPTIONS \
 "
 
 cmake $CMAKE_OPTIONS
-cmake --build ${BUILD_DIR} --config Release --clean-first
+cmake --build ${BUILD_DIR} --clean-first
 ctest --test-dir ${BUILD_DIR} --verbose --no-tests=error
