af9161394eeda3a247a3e6c198b7478c32985dcd
krazijames <krazijames@gmail.com>
2023-03-22 14:10:06 +0900

    Rename confusing variable

--- build-static-lib.sh
+++ build-static-lib.sh
@@ -5,7 +5,7 @@ set -e
 SOURCE_DIR=static-lib
 BUILD_DIR=build/static-lib
 OUTPUT_DIR=outputs/static-lib
-ONNXRUNTIME_ROOT=onnxruntime
+ONNXRUNTIME_SOURCE_DIR=onnxruntime
 
 case $(uname -s) in
 Darwin)
@@ -25,14 +25,14 @@ cmake \
     -S $SOURCE_DIR \
     -B $BUILD_DIR \
     -D CMAKE_BUILD_TYPE=Release \
-    -D ONNXRUNTIME_ROOT=$ONNXRUNTIME_ROOT
+    -D ONNXRUNTIME_SOURCE_DIR=$ONNXRUNTIME_SOURCE_DIR
 cmake --build $BUILD_DIR --config Release -j $NUM_PARALLEL_JOBS
 cmake --install $BUILD_DIR --config Release --prefix $OUTPUT_DIR
 
 cmake \
     -S $SOURCE_DIR/tests \
     -B $BUILD_DIR/tests \
-    -D ONNXRUNTIME_ROOT=$ONNXRUNTIME_ROOT \
+    -D ONNXRUNTIME_SOURCE_DIR=$ONNXRUNTIME_SOURCE_DIR \
     -D ONNXRUNTIME_LIB_DIR=$OUTPUT_DIR/lib
 cmake --build $BUILD_DIR/tests --clean-first
 ctest --test-dir $BUILD_DIR/tests --verbose --no-tests=error
--- build-wasm-static-lib.sh
+++ build-wasm-static-lib.sh
@@ -5,8 +5,8 @@ set -e
 LIB_NAME=${LIB_NAME:=onnxruntime_webassembly}
 BUILD_DIR=build/wasm-static-lib
 OUTPUT_DIR=${OUTPUT_DIR:=outputs/wasm-static-lib}
-ONNXRUNTIME_ROOT=onnxruntime
-EMSDK_DIR=$ONNXRUNTIME_ROOT/cmake/external/emsdk
+ONNXRUNTIME_SOURCE_DIR=onnxruntime
+EMSDK_DIR=$ONNXRUNTIME_SOURCE_DIR/cmake/external/emsdk
 BUILD_OPTIONS="\
     --build_dir $BUILD_DIR \
     --config Release \
@@ -27,12 +27,12 @@ source $EMSDK_DIR/emsdk_env.sh
 
 rm -f $BUILD_DIR/Release/libonnxruntime_webassembly.a
 
-$ONNXRUNTIME_ROOT/build.sh $BUILD_OPTIONS
+$ONNXRUNTIME_SOURCE_DIR/build.sh $BUILD_OPTIONS
 
 mkdir -p $OUTPUT_DIR/include
-cp $ONNXRUNTIME_ROOT/include/onnxruntime/core/session/onnxruntime_c_api.h $OUTPUT_DIR/include
-cp $ONNXRUNTIME_ROOT/include/onnxruntime/core/session/onnxruntime_cxx_api.h $OUTPUT_DIR/include
-cp $ONNXRUNTIME_ROOT/include/onnxruntime/core/session/onnxruntime_cxx_inline.h $OUTPUT_DIR/include
+cp $ONNXRUNTIME_SOURCE_DIR/include/onnxruntime/core/session/onnxruntime_c_api.h $OUTPUT_DIR/include
+cp $ONNXRUNTIME_SOURCE_DIR/include/onnxruntime/core/session/onnxruntime_cxx_api.h $OUTPUT_DIR/include
+cp $ONNXRUNTIME_SOURCE_DIR/include/onnxruntime/core/session/onnxruntime_cxx_inline.h $OUTPUT_DIR/include
 
 mkdir -p $OUTPUT_DIR/lib
 cp $BUILD_DIR/Release/libonnxruntime_webassembly.a $OUTPUT_DIR/lib/lib$LIB_NAME.a
@@ -45,7 +45,7 @@ fi
 cmake \
     -S wasm-static-lib/tests \
     -B $BUILD_DIR/tests \
-    -D ONNXRUNTIME_ROOT=onnxruntime \
+    -D ONNXRUNTIME_SOURCE_DIR=onnxruntime \
     -D ONNXRUNTIME_LIB_DIR=$OUTPUT_DIR/lib \
     -D CMAKE_TOOLCHAIN_FILE=$EMSDK/upstream/emscripten/cmake/Modules/Platform/Emscripten.cmake
 cmake --build $BUILD_DIR/tests --clean-first
